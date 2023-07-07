0173e6bcc37f667f34d1f15cd73ab650cf3cc07b
Bruno-FV <brunofv0102@gmail.com>
2023-07-07 11:44:31 -0300

    organizando o codigo

--- src/main/java/com/tcc/laboratorioVida/Controllers/AreaAdminController.java
+++ src/main/java/com/tcc/laboratorioVida/Controllers/AreaAdminController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tcc.laboratorioVida.Models.AgendamentoConsultas;
 import com.tcc.laboratorioVida.Models.AgendamentoExames;
